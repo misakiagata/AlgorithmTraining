0f607d876b069a3566ec2e19bc6ea01b9b3f40da
Misaki Agata <p17004am@gm.tsuda.ac.jp>
2019-07-04 16:17:49 +0900

    Add compressionString.py

---
+++ compressionString.py
@@ -0,0 +1,29 @@
+#文字列圧縮
+#aaabbccc->a3b2c3
+
+def compression(word):
+    word += '%' #ダミーの%を入力された文字列の最後に入れる
+    compList = []
+    print(word)
+    counter = 0
+    index = 0
+    while index < len(word)-1:
+        if word[index] == word[index+1]:
+            counter += 1
+        else:
+            counter += 1
+            compList.append(word[index])
+            compList.append(str(counter))
+            counter = 0
+        index += 1
+    return ''.join(compList)
+
+def main(originWord):
+    processedWord = compression(originWord)
+    if len(processedWord) < len(originWord):
+        return processedWord
+    else:
+        return originWord
+
+input = input('Input word: ')
+print(main(input))
